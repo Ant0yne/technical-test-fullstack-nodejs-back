6efc6c75b3c8ecc81a041e44021034637a28b85e
Ant0yne <antoine.soliv@gmail.com>
2024-04-03 16:29:27 +0200

    basic-routes

---
+++ index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// ROUTES
+const comicsRoutes = require("./routes/comics");
+const charactersRoutes = require("./routes/characters");
+
+// A try catch to contact the DDB
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		const app = express();
+		app.use(cors());
+		app.use(express.json());
+		app.use(comicsRoutes);
+		app.use(charactersRoutes);
+
+		app.all("/", (req, res) => {
+			res.status(200).json({
+				message:
+					"Bienvenue sur ce Test Technique Fullstack ! Tout sur Marvel !",
+			});
+		});
+
+		app.all("*", (req, res) => {
+			res.status(404).json({ message: "Page not found" });
+		});
+
+		app.listen(process.env.PORT, () => {
+			console.log("SERVER ON");
+		});
+	})
+	.catch((error) => {
+		console.error(error.message);
+	});
---
+++ routes/characters.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const express = require("express");
+const axios = require("axios");
+const mongoose = require("mongoose");
+
+const router = express.Router();
+
+// Get the 100 first characters from API (alphabetic order by default) ---------------------------------------------------------------------
+router.get("/characters", async (req, res) => {
+	let nameWOSpace = "";
+	let url = process.env.URL_API + "/characters" + process.env.MARV_API_KEY;
+
+	// All the potential queries received
+	const { name, limit, skip } = req.query;
+
+	// Remove the possible between word to add %20 (better for HTTP query)
+	name && (nameWOSpace = name.replace(" ", "%20"));
+
+	console.log(nameWOSpace);
+
+	// Conditions of errors
+	// if wrong type of query
+	if (
+		(name && typeof name !== "string") ||
+		(limit && isNaN(limit)) ||
+		(skip && isNaN(skip))
+	) {
+		return res.status(400).json({
+			message: "Please use the right type of query.",
+		});
+	}
+
+	// create the url to contact API with queries if there is some
+	name && (url += "&name=" + nameWOSpace);
+	limit && (url += "&limit=" + limit);
+	skip && (url += "&skip=" + skip);
+
+	const response = await axios.get(url);
+
+	return res.status(200).json(response.data);
+});
+
+// Get the 100 first characters from API (alphabetic order by default) -----------------------------------------------------------------------
+router.get("/character/:characterId", async (req, res) => {
+	try {
+		const characterId = req.params.characterId;
+
+		console.log(
+			process.env.URL_API +
+				"/character/" +
+				characterId +
+				process.env.MARV_API_KEY
+		);
+		if (mongoose.isObjectIdOrHexString(characterId) === false) {
+			return res.status(400).json({
+				message: "Please use a valid Id.",
+			});
+		}
+
+		const response = await axios.get(
+			process.env.URL_API +
+				"/character/" +
+				characterId +
+				process.env.MARV_API_KEY
+		);
+
+		return res.status(200).json(response.data);
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
+});
+
+module.exports = router;
